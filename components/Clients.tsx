b4089ca4b8467f428ff6e7302db75465ca53def2
GK <gabykassab0@gmail.com>
2024-12-23 00:54:48 +0200

    frontend : adjusting sections

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -26,13 +26,12 @@ const Clients = () => {
                 ></Image>
                 <h2 className="bold-40 lg:bold-64">Our Clients</h2>
             </div>
-            <ul>
+            <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20 ">
                 {FEATURES.map((feature)=>(
                     <FeatureItem 
                     key={feature.title}
                     title={feature.title} 
                     icon={feature.icon}
-                    variant={feature.variant}
                     description={feature.description}
                     />
                 ))}
@@ -46,19 +45,21 @@ const Clients = () => {
 type featureItem = {
     title: string;
     icon: string;
-    variant: string;
     description: string;
 }
 
-const FeatureItem =({title,icon,variant,description}:featureItem)=>{
+const FeatureItem =({title,icon,description}:featureItem)=>{
     return (
     <li className="flex w-full flex-1 flex-col items-start">
         <div className="rounded-full p-4 lg:p-7">
             <Image src={icon} alt="map" width={28} height={28}/>
         </div>
-
+        <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
             {title}
-        
+        </h2>
+        <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none" >
+        {description}
+        </p>
     </li>
     )
 }
