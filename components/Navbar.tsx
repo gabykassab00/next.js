08a443cbea3474a5d590d443a0c9d37854e65d97
GK <gabykassab0@gmail.com>
2024-12-22 14:08:11 +0200

    frontend : adding logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Navbar = () => {
   return (
     <nav className='border-2 border-red-500 flex-between max-container padding-container relative z-30 py-5'>
         <Link href="/">
+        <Image src="/logo.png" alt='logo' width={74} height={29}></Image>
         </Link>
     </nav>
   )
