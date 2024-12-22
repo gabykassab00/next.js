37c06769b9f5b913327966f5be6e764a31ff2718
GK <gabykassab0@gmail.com>
2024-12-22 14:27:39 +0200

    frontend : adding hover to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,14 +8,18 @@ const Navbar = () => {
     <nav className=" flex-between max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/logo.png" alt="logo" width={74} height={29}></Image>
-        <ul className="hidden h-full gap-12 lg:flex">
-          {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key}>
-              {link.label}
-            </Link>
-          ))}
-        </ul>
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
   );
 };
