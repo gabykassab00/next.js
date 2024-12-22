71291083a89a8649355368048a339a49b99a089c
GK <gabykassab0@gmail.com>
2024-12-23 00:23:09 +0200

    frontend : adding image to clients

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Clients = () => {
   return (
 <section className='border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-    
+    <div className='max-container padding-container relative w-full justify-end'>
+        <div>
+            <Image
+            src=""
+            alt=''
+            width={440}
+            height={1000}
+            className='feauture-phone'
+            >
+
+            </Image>
+        </div>
+
+    </div>
     
 </section>
   )
