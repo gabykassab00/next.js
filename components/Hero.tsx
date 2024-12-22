b225b2a67ee1992fa99c45b50c7da65b4ea52c7c
GK <gabykassab0@gmail.com>
2024-12-22 22:50:46 +0200

    frontend : creating a section for  hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+<div className='hero-map'>
+{/*left */ }
+
+</div>
+</section>
   )
 }
 
