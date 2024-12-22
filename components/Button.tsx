20dd542829b514fa16189258ece5570f6bedcac3
GK <gabykassab0@gmail.com>
2024-12-22 15:08:51 +0200

    frontend : adding the blue button

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,9 @@ type buttonprops = {
 };
 
 const Button = ({ type, title, icon, variant }: buttonprops) => {
-  return <button type={type}>
+  return <button
+  className={`flexCenter gap-3 rounded-full border ${variant}`}
+  type={type}>
     {icon && <Image src={icon} alt={title} width={24} height={24}></Image>}
     <label className="bold-16 whitespace-nowrap">{title}</label>
   </button>;
