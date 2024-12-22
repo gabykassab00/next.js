1f442dd019f06d341927192f47e36f1791fcef0b
GK <gabykassab0@gmail.com>
2024-12-22 23:15:37 +0200

    frontend : adding titles in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,12 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1></h1>
+        <h1 className="bold-52 lg:bold-88 text-blue">AIPRO,<br></br>OFFICIALLY RELEASED</h1>
+        <h2>THE SMARTEST AI ANALYZER</h2>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px] ">
+        The smartest Ai Analyzer and chosen by many top professional clubs to capture,
+        review,share and analyze your games & training sessions
+        </p>
       </div>
     </section>
   );
