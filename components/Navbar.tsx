8dfca450ab9f9f26d7615d7e2279455ea8b28a72
GK <gabykassab0@gmail.com>
2024-12-22 14:24:38 +0200

    frontend : link the navbar with the index

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,23 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className=' flex-between max-container padding-container relative z-30 py-5'>
-        <Link href="/">
-        <Image src="/logo.png" alt='logo' width={74} height={29}></Image>
-        <ul className='hidden h-full gap-12 lg:flex'>
-        
+    <nav className=" flex-between max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={74} height={29}></Image>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.label}
+            </Link>
+          ))}
         </ul>
-        </Link>
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
