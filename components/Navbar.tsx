d7bd17b6164ba9c39cb0be95a70eb2cf7773c369
GK <gabykassab0@gmail.com>
2024-12-22 13:59:01 +0200

    frontend : emptying public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <nav className='border-2 border-red-500 flex-between max-container padding-container relative z-30 py-5'>
+        <Link href="/">
+        </Link>
+    </nav>
   )
 }
 
