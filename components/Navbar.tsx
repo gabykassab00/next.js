1c26e8430dd8715d97cea363e7e610cbda7a56a9
GK <gabykassab0@gmail.com>
2024-12-22 14:21:09 +0200

    frontend : adding the navbar labels

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='border-2 border-red-500 flex-between max-container padding-container relative z-30 py-5'>
+    <nav className=' flex-between max-container padding-container relative z-30 py-5'>
         <Link href="/">
         <Image src="/logo.png" alt='logo' width={74} height={29}></Image>
+        <ul className='hidden h-full gap-12 lg:flex'>
+        
+        </ul>
         </Link>
     </nav>
   )
---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how are model works', label: 'WorkFlow' },
+    { href: '/', key: 'our latest new and updates', label: 'Updates' },
+    { href: '/', key: 'our clients', label: 'Clients ' },
+    { href: '/', key: 'giving feedback', label: 'FeedBack' },
+  ];
+  
+
+  
