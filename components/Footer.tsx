e385085d03ef4efe0ad51c9857cf77a1978e18b4
GK <gabykassab0@gmail.com>
2024-12-23 21:45:09 +0200

    frontend : adjusting css in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,18 +12,17 @@ const Footer = () => {
             <Image src="/logo.png" alt="logo" width={74} height={29} />
           </Link>
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((columns)=>(
-                <Footercolumn title={columns.title}>
-                    <ul className="regular-14 felx flex-col gap-4 text-gray-30">
-                    {columns.links.map((link)=>(
-                        <Link href="/" key={link}>
-                            {link}
-                        </Link>
-                    ))}
-                    </ul>
-                </Footercolumn>
+            {FOOTER_LINKS.map((columns) => (
+              <Footercolumn title={columns.title}>
+                <ul className="regular-14 felx flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </Footercolumn>
             ))}
-
           </div>
         </div>
       </div>
@@ -31,17 +30,18 @@ const Footer = () => {
   );
 };
 
-export default Footer;
-
 type Footercolumnprops = {
-    title: string;
-    children: React.ReactNode;
-}
+  title: string;
+  children: React.ReactNode;
+};
 
-const Footercolumn =({title,children}:Footercolumnprops)=>{
-        return (
-            <div>
-                <h4>{title}</h4>
-            </div>
-        )
-}
+const Footercolumn = ({ title, children }: Footercolumnprops) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
