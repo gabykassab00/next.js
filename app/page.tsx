d053fb4223ef0c0d194266719b477b803e6b12cd
GK <gabykassab0@gmail.com>
2024-12-24 16:37:19 +0200

    frontend : adding title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Clients from "@/components/Clients";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Workflow from "@/components/Workflow";
 
 export default function Home() {
   return (
     <>
     <Hero></Hero>
+    <Workflow></Workflow>
     <Clients></Clients>
     <Footer></Footer>
     </>
--- components/Workflow.tsx
+++ components/Workflow.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Workflow = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <h4 className='uppercase font-semibold mb-1 text-red-700-xl'>
+        {`Cameras,Analysis Software and Data for sport`}
+      </h4>
     </div>
   )
 }
