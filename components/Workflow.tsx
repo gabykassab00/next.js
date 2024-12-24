678ea379862720a994d868f60c0a3319526f7b76
GK <gabykassab0@gmail.com>
2024-12-24 16:41:33 +0200

    frontend : adding second title

--- components/Workflow.tsx
+++ components/Workflow.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Workflow = () => {
   return (
     <div className='container'>
-      <h4 className='uppercase font-semibold mb-1 text-red-700-xl'>
+      <h4 className='uppercase font-semibold text-center mb-1 text-red-700-xl'>
         {`Cameras,Analysis Software and Data for sport`}
       </h4>
+    <h2 className='text-center font-semibold text-5xl mt-5'>Designed to improve any team’s workflow</h2>
+      <div>section</div>
     </div>
   )
 }
