18901d374e67a9cc9930538f5933e1a268c0fe02
GK <gabykassab0@gmail.com>
2024-12-26 20:25:58 +0200

    frontend : adding a rating

--- components/Review.tsx
+++ components/Review.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import 'react-icons/fa'
+import { FaStar } from 'react-icons/fa'
 const Review = () => {
   return (
     <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#1976d2]'>
@@ -13,10 +14,17 @@ const Review = () => {
                 <div>
                     <p className='text-2xl font-bold text-white'>4.88</p>
                     <p className='text-white mb-2'>Overall Rating</p>
-                    
+                    <div className='flex items-center'>
+                        <FaStar className='text-white' />
+                        <FaStar className='text-white' />
+                        <FaStar className='text-white' />
+                        <FaStar className='text-white' />
+                        <FaStar className='text-white' />
+                    </div>
                 </div>
             </div>
         </div>
+        {/* */}
       </div>
     </div>
   )
