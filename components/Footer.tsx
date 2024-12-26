71c0b87db6cde4ab3d7a8e8ec79111aba0b53b67
GK <gabykassab0@gmail.com>
2024-12-26 11:04:40 +0200

    frontend : initialize udpate

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,8 +52,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="border bg-gray-20"/>
-        <p className="regular-14 w-full text-center text-gray-30">2025 AIPRO | all rights reserved</p>
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">
+          2025 AIPRO | all rights reserved
+        </p>
       </div>
     </footer>
   );
