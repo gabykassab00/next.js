60efcc71857ae15b3d4dab28faed96e3700bea54
GK <gabykassab0@gmail.com>
2024-12-26 20:20:14 +0200

    frontend : adding a title

--- components/Review.tsx
+++ components/Review.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Review = () => {
   return (
     <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#1976d2]'>
-      
+      <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
+        <div>
+            <h1 className='text-2xl font-semibold text-white'>FeedBack</h1>
+        </div>
+      </div>
     </div>
   )
 }
