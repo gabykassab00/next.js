0e1813be27b494944381075a8317bb6a214b2fad
GK <gabykassab0@gmail.com>
2024-12-26 21:36:56 +0200

    frontend : adding datat in index to use in reviews

--- constants/index.ts
+++ constants/index.ts
@@ -77,5 +77,78 @@ export const SOCIALS = {
   ],
 };
   
-
+export const reviewData = [
+  {
+    id: 1,
+    name: "Douglas Wright",
+    review:
+      "The service exceeded my expectations. The team was professional, and I couldn't be happier with the results. Highly recommended to everyone!",
+    image: "/images/u1.jpg",
+    rank:"Head of Academy Analysis",
+    club:"crystal palace",
+  },
+  {
+    id: 2,
+    name: "Andreas Neuendorf",
+    review:
+      "AIPRO edits all of the match videos. This means that shortly after the game we can provide all the boys with all scenes and we as coaches can analyse the game more quickly.",
+    image: "/images/u2.jpg",
+    rank:"Former Assistant Coach",
+    club:"Hertha BSC",
+  },
+  {
+    id: 3,
+    name: "Rob Smith",
+    review:
+      "The AIPRO platform provides us with data insights that allows us to understand performance better, both on an individual game basis as well as by identifying trends over a period of time.",
+    image: "/images/u3.jpg",
+    rank:"Lead Academy Analyst",
+    club:"Watford"
+  },
+  {
+    id: 4,
+    name: "Andrea Sentimenti",
+    review:
+      "The AI software provided by AIPRO works really well, I can save a lot of time and it allows me to focus on other aspects of the analysis.",
+    image: "/images/u1.jpg",
+    rank:"Goalkeeping Coach",
+    club:"Bologna F.C. 1909",
+  },
+  {
+    id: 5,
+    name: "Takeshi Okada",
+    review:
+      "I believe in the importance of performance data and video in modern football. AIPRO's platform includes everything we need and more.",
+    image: "/images/u2.jpg",
+    rank:"president",
+    club:"FC Imabari"
+  },
+  {
+    id: 6,
+    name: "Kim Dowan",
+    review:
+      "It is good to analyse the overall movement and shape of the team, for example to identify areas that were not defended during training. The players are asked to check and watch the video every day.",
+    image: "/images/u3.jpg",
+    rank:"Head Coach",
+    club:"HANAoneQ",
+  },
+  {
+    id: 7,
+    name: "Holger Fuchs",
+    review:
+      "The BEPRO videos are shared with the clubs at a very high quality alongside data, so there is a huge time saving.",
+    image: "/images/u1.jpg",
+    rank:"CEO",
+    club:"Regionalliga Nordost",
+  },
+  {
+    id: 8,
+    name: "João Alves",
+    review:
+      "It's very important to us that the players can just open their phone and go to their groups and open their individual clips.",
+    image: "/images/u2.jpg",
+    rank:"Assistant Coach",
+    club:"Notts County",
+  },
+];
 
