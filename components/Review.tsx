b02332e1d62cec21c2a345f90e7632df378a9d43
GK <gabykassab0@gmail.com>
2024-12-26 20:23:01 +0200

    frontend : adding a paragraph

--- components/Review.tsx
+++ components/Review.tsx
@@ -9,6 +9,13 @@ const Review = () => {
             <p className='mt-6 text-gray-200'>
             Let’s hear what people say about AIPRO!
             </p>
+            <div className='mt-6 flex items-center space-x-6'>
+                <div>
+                    <p className='text-2xl font-bold text-white'>4.88</p>
+                    <p className='text-white mb-2'>Overall Rating</p>
+                    
+                </div>
+            </div>
         </div>
       </div>
     </div>
