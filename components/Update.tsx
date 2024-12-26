6c1d255921e63c7ab49d9d9379acebe36d16f73f
GK <gabykassab0@gmail.com>
2024-12-26 11:20:48 +0200

    frontend : adding first picture

--- components/Update.tsx
+++ components/Update.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Update = () => {
@@ -6,8 +7,13 @@ const Update = () => {
       <h2 className="text-center font-semibold text-5xl mt-5">
         Our Latest News & Updates 
       </h2>
-      <div className="grid grid-cols-4 gap-5 mt-10">
-        
+      <div className="grid grid-cols-3 gap-5 mt-10">
+        <div className="flex shadow-md h-96 flex-col0" style={{height:"500px"}}>
+            <div className="relative w-full">
+                <Image src="/editor.png" alt="upload" className="object-cover" layout="fill"/>
+            </div>
+        </div>
+
       </div>
     </div>
   );
