6cd9e7e8c2e7f299091d711b9da3a14dd6f10833
GK <gabykassab0@gmail.com>
2024-12-26 22:07:13 +0200

    frontend : addjusted images

--- constants/index.ts
+++ constants/index.ts
@@ -83,7 +83,7 @@ export const reviewData = [
     name: "Douglas Wright",
     review:
       "The service exceeded my expectations. The team was professional, and I couldn't be happier with the results. Highly recommended to everyone!",
-    image: "/images/u1.jpg",
+    image: "/douglas.png",
     rank:"Head of Academy Analysis",
     club:"crystal palace",
   },
@@ -92,7 +92,7 @@ export const reviewData = [
     name: "Andreas Neuendorf",
     review:
       "AIPRO edits all of the match videos. This means that shortly after the game we can provide all the boys with all scenes and we as coaches can analyse the game more quickly.",
-    image: "/images/u2.jpg",
+    image: "/andreas.jpg",
     rank:"Former Assistant Coach",
     club:"Hertha BSC",
   },
@@ -101,7 +101,7 @@ export const reviewData = [
     name: "Rob Smith",
     review:
       "The AIPRO platform provides us with data insights that allows us to understand performance better, both on an individual game basis as well as by identifying trends over a period of time.",
-    image: "/images/u3.jpg",
+    image: "/rob.jpeg",
     rank:"Lead Academy Analyst",
     club:"Watford"
   },
@@ -110,7 +110,7 @@ export const reviewData = [
     name: "Andrea Sentimenti",
     review:
       "The AI software provided by AIPRO works really well, I can save a lot of time and it allows me to focus on other aspects of the analysis.",
-    image: "/images/u1.jpg",
+    image: "/sentimenti.png",
     rank:"Goalkeeping Coach",
     club:"Bologna F.C. 1909",
   },
@@ -119,7 +119,7 @@ export const reviewData = [
     name: "Takeshi Okada",
     review:
       "I believe in the importance of performance data and video in modern football. AIPRO's platform includes everything we need and more.",
-    image: "/images/u2.jpg",
+    image: "/takeshi.jpg",
     rank:"president",
     club:"FC Imabari"
   },
@@ -128,7 +128,7 @@ export const reviewData = [
     name: "Kim Dowan",
     review:
       "It is good to analyse the overall movement and shape of the team, for example to identify areas that were not defended during training. The players are asked to check and watch the video every day.",
-    image: "/images/u3.jpg",
+    image: "/kim.jpg",
     rank:"Head Coach",
     club:"HANAoneQ",
   },
@@ -137,7 +137,7 @@ export const reviewData = [
     name: "Holger Fuchs",
     review:
       "The BEPRO videos are shared with the clubs at a very high quality alongside data, so there is a huge time saving.",
-    image: "/images/u1.jpg",
+    image: "/holger.jpg",
     rank:"CEO",
     club:"Regionalliga Nordost",
   },
@@ -146,7 +146,7 @@ export const reviewData = [
     name: "João Alves",
     review:
       "It's very important to us that the players can just open their phone and go to their groups and open their individual clips.",
-    image: "/images/u2.jpg",
+    image: "/joa.jpg",
     rank:"Assistant Coach",
     club:"Notts County",
   },
