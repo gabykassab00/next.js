d1792314982e84e2ab1cb1ff3049ae348b8c16dd
GK <gabykassab0@gmail.com>
2024-12-28 10:52:37 +0200

    frontend : creating the popup

--- components/Login.tsx
+++ components/Login.tsx
@@ -17,25 +17,14 @@ const Login = ({handleshowlogin}:{handleshowlogin:()=>void}) => {
         e.preventDefault();
         console.log(signup?"sign up success":"login success");
     }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
-    <div>
-      
+    <div className='w-full h-full absolute top-0 backdrop-filter backdrop-brightness-75 backdrop-blur-md flex justify-center items-center'onClick={handleshowlogin}>
+      <div onClick={(e)=>e.stopPropagation()} className='relative bg-white rounded-lg shadow px-16'>
+        <button type='button' className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center popup-close' onClick={handleshowlogin}>
+            
+        </button>
+      </div>
     </div>
   )
 }
