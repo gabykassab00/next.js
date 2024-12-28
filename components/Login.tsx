f9629485a6291065c266fafc52c7171c3bf30a03
GK <gabykassab0@gmail.com>
2024-12-28 10:53:10 +0200

    frontend : creating the close svg

--- components/Login.tsx
+++ components/Login.tsx
@@ -22,7 +22,9 @@ const Login = ({handleshowlogin}:{handleshowlogin:()=>void}) => {
     <div className='w-full h-full absolute top-0 backdrop-filter backdrop-brightness-75 backdrop-blur-md flex justify-center items-center'onClick={handleshowlogin}>
       <div onClick={(e)=>e.stopPropagation()} className='relative bg-white rounded-lg shadow px-16'>
         <button type='button' className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center popup-close' onClick={handleshowlogin}>
-            
+        <svg aria-hidden="true" className="w-5 h-5" fill="#1976d2" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+          </svg>
         </button>
       </div>
     </div>
