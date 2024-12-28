af647e052dd51b3dbc314928725cb221c6e744be
GK <gabykassab0@gmail.com>
2024-12-28 10:55:45 +0200

    frontend : creating default text between login and sign up

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,3 +1,4 @@
+import { sign } from 'crypto';
 import React, { useState } from 'react'
 
 const Login = ({handleshowlogin}:{handleshowlogin:()=>void}) => {
@@ -26,6 +27,12 @@ const Login = ({handleshowlogin}:{handleshowlogin:()=>void}) => {
             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
           </svg>
         </button>
+
+        <div className='text-center'>
+            <p className='mb-3 text-2xl font-semibold leading-5 text-slate-900'>
+                {signup?'create your account':'login to your account'}
+            </p>
+        </div>
       </div>
     </div>
   )
