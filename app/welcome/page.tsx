66c2fb4d072feebe742eace300e97c7ebb49973e
GK <gabykassab0@gmail.com>
2024-12-31 17:07:01 +0200

    frontend : initialized the authentication function

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { useRouter } from 'next/router';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Page = () => {
   const[authenticated,setauthenticated] = useState(false);
   const router = useRouter();
 
-  
+    useEffect(()=>{
+      const authentication = async ()=>{
+        try {
+          
+        }
+      }
+    })
 
   return (
     <div>
