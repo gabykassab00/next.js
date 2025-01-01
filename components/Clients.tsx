3ef83095e2e4823a24d0c03585e5270f5dd15f4b
GK <gabykassab0@gmail.com>
2025-01-01 23:38:27 +0200

    frontend : adding client section style

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,10 +1,10 @@
 import { FEATURES } from "@/constants";
 import Image from "next/image";
 import React from "react";
-
+import "../styles/clients.css"
 const Clients = () => {
   return (
-    <section className=" flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+    <section className="clients-section">
       <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
