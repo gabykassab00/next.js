eb09198034855a15031eeb36e75d30ebe88454f9
GK <gabykassab0@gmail.com>
2025-01-02 13:45:04 +0200

    frontend : added footer container

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import '../styles/footer.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="padding-container max-container flex w-full flex-col gap-14">
+      <div className="footer-container">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
             <Image src="/logo.png" alt="logo" width={74} height={29} />
