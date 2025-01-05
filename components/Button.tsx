cade84ae3ec9ee4fb7745937523e8e298b96760c
GK <gabykassab0@gmail.com>
2025-01-06 01:00:07 +0200

    frontend: adjusted buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,9 @@ const Button = ({ type, title, icon, variant,onClick }: buttonprops) => {
   return <button
   className={`flexCenter gap-3 rounded-full border ${variant}`}
   type={type}
-  onClick={onClick}>
+  onClick={onClick}
+  style={{ padding: '4px 12px', fontSize: '12px', height: '52px' }}
+  >
     {icon && <Image src={icon} alt={title} width={24} height={24}></Image>}
     <label className="bold-16 whitespace-nowrap">{title}</label>
   </button>;
