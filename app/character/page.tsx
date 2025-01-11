9d648fbf92fe9ca86936e130e43838c3bb0fd74e
GK <gabykassab0@gmail.com>
2025-01-12 01:22:06 +0200

    frontend : added backgorund and go back button

--- app/character/page.tsx
+++ app/character/page.tsx
@@ -1,9 +1,18 @@
 "use client";
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const page = () => {
+  const router = useRouter()
   return (
-    <div>
+    <div className='min-h-screen flex items-center justify-center bg-cover bg-center' style={{backgroundImage:"url('/background-pic.jpg')"}}>
+
+      <button 
+      onClick={()=>router.push('/upload')}
+      className='absolute top-4 right-4 bg-white text-blue-700 rounded-lg py-2 px-4'>
+        Go Back
+      </button>
+      
       
     </div>
   )
