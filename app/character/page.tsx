66ab5cc71a7ed11ecdc2526f8468bca3452c5992
GK <gabykassab0@gmail.com>
2025-01-12 01:24:35 +0200

    frontend : added player button

--- app/character/page.tsx
+++ app/character/page.tsx
@@ -12,8 +12,18 @@ const page = () => {
       className='absolute top-4 right-4 bg-white text-blue-700 rounded-lg py-2 px-4'>
         Go Back
       </button>
-      
-      
+
+      <div className='bg-white p-8 rounded shadow w-96'>
+        <h1 className='text-xl font-bold mb-6 text-center'>are you a player or a coach ?</h1>
+
+        <div className='space-y-4'>
+          <button 
+          onClick={()=>alert("Player selected")}
+          className='bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-600'>
+            Player
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
