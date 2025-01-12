b63906be47421f5be8cf4c67039e1b6048606282
GK <gabykassab0@gmail.com>
2025-01-12 12:20:11 +0200

    backend : fetched player 1 stats added to table

--- app/player1/page.tsx
+++ app/player1/page.tsx
@@ -47,8 +47,40 @@ const page = () => {
     },[])
 
   return (
-    <div>
-      
+    <div className='min-h-screen flex flex-col items-center justify-center bg-cover bg-center' style={{backgroundImage:"url('/background-pic.jpg')"}}>
+        <button onClick={()=>router.push('/player')} className='absolute top-4 right-4 bg-white text-blue-700 rounded-lg py-2 px-4 shadow-lg hover:bg-blue-100'>
+            Go Back
+        </button>
+
+        <div className='bg-white p-6 rounded shadow w-4/5 max-w-4xl'>
+        <h1 className='text-3xl font-bold mb-6 text-center text-gray-700'>
+            team 1 player stats
+        </h1>
+
+        <div className='overflow-x-auto'>
+            <table className='min-w-full table-auto bg-white border border-gray-300 rounded-lg shadow-lg'>
+                <thead>
+                    <tr className='bg-gray-200 text-gray-600 uppercase text-sm leading-normal'>
+                        <th className='py-3 px-6 text-left'>Player ID</th>
+                        <th className='py-3 px-6 text-left'>Average Speed (km/h)</th>
+                        <th className='py-3 px-6 text-left'>total distance (m)</th>
+                        <th className='py-3 px-6 text-left'>total passes</th>
+
+                    </tr>
+                </thead>
+                <tbody className='text-gray-600 text-sm font-light '>
+                    {team1PlayerStats.map((player,index)=>(
+                        <tr key={index} className='border-b border-gray-200 hover:bg-gray-100'>
+                            <td className='py-3 px-6 text-left'>{player.playerId}</td>
+                            <td className='py-3 px-6 text-left'>{player.averageSpeed.toFixed(2)}</td>
+                            <td className='py-3 px-6 text-left'>{player.totalDistance.toFixed(2)}</td>
+                            <td className='py-3 px-6 text-left'>{player.totalPasses}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        </div>      
     </div>
   )
 }
